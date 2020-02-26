a56e6a31edd2058431b6b0da3bb6d287a78c79f5
Alex Watkins <afw821@gmail.com>
2020-02-25 19:08:18 -0500

    First commit emailer

---
+++ example.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_ADDRESS,
+        pass: process.env.EMAIL_PASSWORD
+    }
+});
+
+var mailOptions = {
+    from: process.env.EMAIL_ADDRESS,
+    to: 'afw821@yahoo.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+});
